234dea11c5a199b3c99e393bb55188458b3eb50f
unknown <zainabbasashtar55@gmail.com>
2025-10-19 03:53:35 -0700

    Fixed navigation bar issue

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,27 +1,27 @@
 <head>
-    
+
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
-   
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <nav class="navbar navbar-expand-lg bg-white border-bottom mt-3">
     <div class="container">
 
-        
+
         <a class="navbar-brand fw-semibold fs-2" href="{{ route('dashboard') }}"
             style="font-family: 'Lato'; color: rgb(80,74,74);">
             ZIPPER
         </a>
 
-       
+
         <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar"
             aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        
+
         <div class="collapse navbar-collapse" id="mainNavbar">
             <ul class="navbar-nav ms-auto fw-bold fs-5 align-items-lg-center" style="margin-top: -0px">
                 <li class="nav-item">
@@ -44,7 +44,7 @@
                     <a class="nav-link text-dark" href="/contact">Contact</a>
                 </li>
 
-                
+
                 <li class="nav-item">
                     <a class="nav-link text-dark" href="#" data-bs-toggle="offcanvas"
                         data-bs-target="#cartSidebar" aria-controls="cartSidebar">
